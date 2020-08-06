fcc2cb29fc30c6ae86942afeba74db512067a80e
wuyun <wuyun@zqlian.com>
2020-08-06 14:54:06 +0800

    zqlian-cli 优化

--- action.js
+++ action.js
@@ -1,15 +1,12 @@
 let utils = require('@zqlianweb/utils')
 let useActions = ['init', 'update', 'upgrade']
-async function getAction (action) {
-  if (action && useActions.indexOf(action)) {
-    return Promise.reject('无效的action，目前支持' + useActions.join())
-  }
-  if (action === 'init') {
-    utils.config.init()
-  }
-}
 module.exports = {
-  async getAction (action) {
-    return getAction(action)
+  async checkAction (action) {
+    if (action && useActions.indexOf(action) === -1) {
+      return Promise.reject('无效的action，目前支持' + useActions.join())
+    }
+    if (action === 'init') {
+      utils.config.init()
+    }
   }
 }
--- index.js
+++ index.js
@@ -12,29 +12,37 @@ const inquirer = require('inquirer')
 const path = require('path')
 const moduleModule = require('./module')
 const actionModule = require('./action')
+async function doAction (action, module, more) {
+  // 进行 action 初始化
+  await actionModule.checkAction(action)
+  // 进行 module 初始化
+  module = await moduleModule.getModule(module)
+  let useModule = require('@zqlianweb/' + module)
+  if (useModule.apply) await useModule.apply(action, more)
+  console.log('执行完成！')
+}
 async function start () {
   program
     .command('init [module]')
     .option('--more [more]', '更多参数')
     .description('zqlian模块初始化')
+    .action(async (module, options) => {
+      doAction('init', module, options.more)
+    })
   program
     .command('update [module]')
     .option('--more [more]', '更多参数')
     .description('zqlian模块更新')
+    .action(async (module, options) => {
+      doAction('update', module, options.more)
+    })
   program
     .command('upgrade [module]')
     .option('--more [more]', '更多参数')
     .description('zqlian模块升级')
+    .action(async (module, options) => {
+      doAction('upgrade', module, options.more)
+    })
   program.parse(process.argv)
-  console.log('program.args', program.args)
-  let action = program.args[0] || 'init'
-  // 进行 action 初始化
-  await actionModule.getAction(action)
-  let module = program.args[1]
-  // 进行 module 初始化
-  module = await moduleModule.getModule(module)
-  let useModule = require('@zqlianweb/' + module)
-  if (useModule.apply) await useModule.apply(action, program.more)
-  console.log('执行完成！')
 }
 start()
